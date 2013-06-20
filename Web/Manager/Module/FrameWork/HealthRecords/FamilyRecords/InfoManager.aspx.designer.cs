f23860e0828cfab8112b87cc224e4d34163a49d3
钟福亮 <zfl924@qq.com>
2013-06-21 00:36:38 +0800

    个人健康档案，新增体检记录

--- Web/Manager/Module/FrameWork/HealthRecords/FamilyRecords/InfoManager.aspx.designer.cs
+++ Web/Manager/Module/FrameWork/HealthRecords/FamilyRecords/InfoManager.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace FrameWork.web.Module.FrameWork.HealthRecords.FamilyRecords {
         protected global::System.Web.UI.WebControls.TextBox F_FamilyCode;
         
         /// <summary>
+        /// F_FamilyCode_label 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label F_FamilyCode_label;
+        
+        /// <summary>
         /// F_UserID 控件。
         /// </summary>
         /// <remarks>
